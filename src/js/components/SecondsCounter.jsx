4f2d89f01c89d5df5cde19a19db567ff7b376d79
Frank Perez <21010185+fperez028@users.noreply.github.com>
2025-06-16 23:16:34 +0000

    basic layout

--- src/js/components/SecondsCounter.jsx
+++ src/js/components/SecondsCounter.jsx
@@ -1,20 +1,23 @@
 import React from "react";
 
 //include images into your bundle
-//import rigoImage from "../../img/rigo-baby.jpg";
+import clockIcon from "../../img/clock-regular.svg";
 
 //create your first component
 export const SecondsCounter = () => {
 	return (
         <div className="d-flex flex-column">
             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" crossOrigin="anonymous" />
-            <div className="container py-5">
-                <div className="p-4 shadow bg-body-secondary rounded-3">
-                <div className="m-4">
-                    <h1 className="display-2">Title</h1>
-                    <p className="fs-5">Description Text</p>
+            <div className="container py-4">
+                <div className="row p-2 shadow bg-dark rounded-3 text-center">
+                    <div className="col m-4 bg-body-secondary"><img src={clockIcon}/></div>
+                    <div className="col m-4 bg-body-secondary"><h1 className="display-2">0</h1></div>
+                    <div className="col m-4 bg-body-secondary"><h1 className="display-2">0</h1></div>
+                    <div className="col m-4 bg-body-secondary"><h1 className="display-2">0</h1></div>
+                    <div className="col m-4 bg-body-secondary"><h1 className="display-2">0</h1></div>
+                    <div className="col m-4 bg-body-secondary"><h1 className="display-2">0</h1></div>
+                    <div className="col m-4 bg-body-secondary"><h1 className="display-2">0</h1></div>
                 </div>
-            </div>
             </div>
         </div>
 	);
