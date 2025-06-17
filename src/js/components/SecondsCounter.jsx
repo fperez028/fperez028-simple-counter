a7a60fdf5967e11f42e9aa29d8cdf0b780691bf9
Frank Perez <21010185+fperez028@users.noreply.github.com>
2025-06-17 00:24:13 +0000

    adjusted icon image size

--- src/js/components/SecondsCounter.jsx
+++ src/js/components/SecondsCounter.jsx
@@ -13,8 +13,8 @@ export const SecondsCounter = ({ seconds }) => {
             />
             <div className="container py-4">
                 <div className="row p-2 bg-dark rounded-3">
-                    <div className="col m-4 bg-secondary rounded-3">
-                        <img src={clockIcon} alt="Clock Icon" />
+                    <div className="col m-4 bg-secondary rounded-3 d-flex align-items-center justify-content-center">
+                        <img src={clockIcon} alt="Clock Icon" className="img-fluid" style={{ maxWidth: '70%' }} />
                     </div>
                     {digits.map((digit, index) => (
                         <div key={index} className="col m-4 bg-secondary rounded-3 d-flex align-items-center justify-content-center">
